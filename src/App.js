51b45335a3a8d65304ab78a57314a444dcebbfc8
Yaqubboy <yaqubboyyaqubboyev45@gmail.com>
2022-04-24 16:17:21 +0500

    sidebar arrived

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React, { useState } from 'react'
 import './App.css'
 import Header from './components/header/index'
 
 function App() {
   // authentication
+  const [user, setUser] = useState({
+    displayName: 'David Rakosi',
+    email: 'david@cleverprogrammer.com',
+    emailVerified: true,
+    phoneNumber: null,
+    photoURL:
+      'https://lh6.googleusercontent.com/-KyLTWqvDIHQ/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclcWGWqkt6YUAan32YO4CSR07Y2jw/s96-c/photo.jpg',
+  })
   return (
     <div className='App'>
-      <Header />
+      <Header userPhoto={user.photoURL} />
       {/* authenticated
         sidebar
         files view
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,49 @@
 import React from 'react'
 import googleDriveLogo from '../../media/google-drive-logo.png'
+import { GoSearch } from 'react-icons/go'
+import { MdExpandMore, MdSettings, MdOutlineApps } from 'react-icons/md'
+import { BiHelpCircle } from 'react-icons/bi'
+import '../../styles/Header.css'
 
-const index = () => {
+const index = ({ userPhoto }) => {
   return (
     <div className='header'>
       <div className='header_logo'>
         <img src={googleDriveLogo} alt='' />
+        <span>Drive</span>
       </div>
-      <div className='header_search-container'></div>
-      <div className='header_icons'></div>
+      <div className='header_search-container'>
+        <div className='header_searchBar'>
+          <GoSearch
+            className='search-icon'
+            style={{ color: 'rgb(82, 82, 82)' }}
+          />
+          <input type='text' placeholder='Search in Drive' />
+          <MdExpandMore style={{ color: 'rgb(82, 82, 82)' }} />
+        </div>
+      </div>
+      <div className='header_icons'>
+        <span>
+          <BiHelpCircle
+            style={{
+              color: 'rgb(82, 82, 82)',
+              fontSize: '28px',
+              margin: '4px',
+            }}
+          />
+          <MdSettings
+            style={{
+              color: 'rgb(82, 82, 82)',
+              fontSize: '28px',
+              margin: '4px',
+            }}
+          />
+        </span>
+        <MdOutlineApps
+          style={{ color: 'rgb(82, 82, 82)', fontSize: '28px', margin: '4px' }}
+        />
+        <img src={userPhoto} alt='User Photo' />
+      </div>
     </div>
   )
 }
